3c83fc7864d8ffbc9fcc9a1325dce21f8ac544ad
3lagenquiz <153177185+3lagenquiz@users.noreply.github.com>
2023-12-13 18:33:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,125 +1,86 @@
 const questions = [
-  {
-     question: "Which is the largest animal in the world?",
-     answers: [
-        { text: "Shark", correct: false},
-        { text: "Blue Whale", correct: true},
-        { text: "Elephant", correct: false},
-        { text: "Giraffe", correct: false},
-       ]
-  },
-  {
-   question: "Wich is the smallest country in the world?",
-     answers: [
-        { text: "Vatican City", correct: true},
-        { text: "Bhutan", correct: false},
-        { text: "Nepal", correct: false},
-        { text: "Shri Lanka", correct: false},
-       ]
-  },
-  {
-     question: "Wich is the largest desert in the wordl?",
-     answers: [
-        { text: "Kalahari", correct: false},
-        { text: "Gobi", correct: false},
-        { text: "Sahara", correct: false},
-        { text: "Antartica", correct: true},
-       ]
-  },
-  {
-     question: "Which is the smallest continent in the world?",
-     answers: [
-        { text: "Asia", correct: false},
-        { text: "Australia", correct: true},
-        { text: "Artctic", correct: false},
-        { text: "Africa", correct: false},
-       ]
-  }
+  // ... (je array met vragen blijft hetzelfde)
 ];
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
-let currentQuestionindex = 0;
+let currentQuestionIndex = 0;
 let score = 0;
 
-function startquiz(){
-  currentQuestionIndex
-  score = 0
+function startQuiz() {
+  currentQuestionIndex = 0; // Initialiseer de variabele currentQuestionIndex
+  score = 0;
   nextButton.innerHTML = "Next";
   showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
   resetState();
   let currentQuestion = questions[currentQuestionIndex];
-  let questionNo = currentQuestionIndex +1;
-  questionElement.innerHTML = questionNo + "." + currentQuestion.
-    question;
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-  currentQuestion.answers.forEach(answer => {
+  currentQuestion.answers.forEach((answer) => {
     const button = document.createElement("button");
-    button.innerHTMl = answer.text;
+    button.innerHTML = answer.text; // Correcte spelling van innerHTML
     button.classList.add("btn");
     answerButtons.appendChild(button);
-    if(answer.correct){
+    if (answer.correct) {
       button.dataset.correct = answer.correct;
     }
     button.addEventListener("click", selectAnswer);
   });
 }
 
-
-function resetState(){
+function resetState() {
   nextButton.style.display = "none";
-  while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButtons.fistChild);
-    }
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
 }
 
-function selcectAnswer(e){
+function selectAnswer(e) {
   const selectBtn = e.target;
   const isCorrect = selectBtn.dataset.correct === "true";
-  if(isCorrect){
-      selectBtn.classList.add("correct");
+  if (isCorrect) {
+    selectBtn.classList.add("correct");
     score++;
-  }else{
+  } else {
     selectBtn.classList.add("incorrect");
   }
-  Array.from(answerButtons.children).forEach(button => {
-    if(button.dataset.correct === "true"){
-      button.classlist.add("correct");
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct"); // Correcte spelling van classList
     }
     button.disabled = true;
   });
   nextButton.style.display = "block";
 }
 
-function showScore(){
+function showScore() {
   resetState();
-  questionElement.innerHTML = `You scored ${score} out of {questions.lenght}!`;
-  nextButton.innerHTML = "Play Again"
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`; // Correcte spelling van length
+  nextButton.innerHTML = "Play Again";
   nextButton.style.display = "block";
 }
 
-function handleNextButton(){
+function handleNextButton() {
   currentQuestionIndex++;
-  if(currentQuestionIndex <questions.lenght){
+  if (currentQuestionIndex < questions.length) {
     showQuestion();
-  }else{
+  } else {
     showScore();
   }
 }
 
-
-nextButton.addEventlistener("click", ()=>{
-  if(currentQuestionIndex < questions.lenght){
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
     handleNextButton();
-  }else{
+  } else {
     startQuiz();
   }
 });
-
 
 startQuiz();
