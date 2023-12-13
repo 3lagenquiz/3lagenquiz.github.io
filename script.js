d2c8ea2605bc5b938244cae3ba85686d51beffcc
3lagenquiz <153177185+3lagenquiz@users.noreply.github.com>
2023-12-13 18:25:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
-ley currentQuestionindex = 0;
+let currentQuestionindex = 0;
 let score = 0;
 
 function startquiz(){
