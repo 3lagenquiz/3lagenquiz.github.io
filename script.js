5a3821fdc49e7603334fea89ab9e0579124cc3af
3lagenquiz <153177185+3lagenquiz@users.noreply.github.com>
2023-12-13 15:35:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+const questions = [
+  {
+     question: "Vraag 1 text",
+     answers: [
+        { text: "goed antwoord", correct: true},
+        { text: "optie antwoord", correct: false},
+        { text: "optie antwoord", correct: false},
+        { text: "optie antwoord", correct: false},
+       ]
+  },
+  {
+   question: "Vraag 2 text",
+     answers: [
+        { text: "goed antwoord", correct: true},
+        { text: "optie antwoord", correct: false},
+        { text: "optie antwoord", correct: false},
+        { text: "optie antwoord", correct: false},
+       ]
+  }
+]
