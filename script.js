cfe99a93e197b64ea516b2878dd25b33537bc906
3lagenquiz <153177185+3lagenquiz@users.noreply.github.com>
2023-12-13 18:54:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,8 +37,6 @@ const questions = [
   }
 ];
 
-];
-
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
